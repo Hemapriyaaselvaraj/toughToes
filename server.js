53df06e37ff7da7c4d10f6eee1d4c665596037cd
man0j111 <manojsv44155@gmail.com>
2025-07-14 20:33:55 +0530

    Signup with google

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const productRoutes = require('./backend/routes/product')
 const connectDB = require('./backend/config/db');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const passport = require('./backend/config/passport');
 
 const app = express();
 require('dotenv').config();
@@ -23,6 +24,9 @@ app.use(session({
   saveUninitialized: true,
   store: MongoStore.create({ mongoUrl: process.env.MONGO_URI })
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('views', path.join(__dirname, 'frontend', 'views'));
 app.set('view engine', 'ejs');
