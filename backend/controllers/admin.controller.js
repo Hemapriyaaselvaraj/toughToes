6abec9fec6ab10e627a42022c6d669fed9195bd4
man0j111 <manojsv44155@gmail.com>
2025-07-16 18:58:14 +0530

    added add-product rout

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -1,10 +1,37 @@
+
 const userModel = require("../models/userModel");
+const ProductCategory = require('../models/productCategoryModel');
+const ProductType = require('../models/productTypeModel');
+const ProductSize = require('../models/productSizeModel');
+const ProductColor = require('../models/productColorModel');
 
 const getDashboard = async (req, res) => {
   const user = await userModel.findOne({ _id: req.session.userId });
   res.render("admin/dashboard", { name: user.firstName});
 };
 
+const getAddProduct = async (req, res) => {
+  try {
+    const categories = await ProductCategory.find({});
+    const types = await ProductType.find({});
+    const sizes = await ProductSize.find({});
+    const colors = await ProductColor.find({});
+    const user = await userModel.findOne({ _id: req.session.userId });
+    res.render('admin/add-product', {
+      mode: 'add',
+      name: user ? user.firstName : '',
+      categories,
+      types,
+      sizes,
+      colors,
+      product: null
+    });
+  } catch (err) {
+    res.status(500).send('Error loading add product page');
+  }
+};
+
 module.exports = {
-  getDashboard
+  getDashboard,
+  getAddProduct
 };
